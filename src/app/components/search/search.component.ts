d6f85a47b06b2313c3e9e4e331f230244fe25f02
Roman Telik <rtelik58@gmail.com>
2023-08-12 23:50:27 +0300

    search-v1

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {
-
+export class SearchComponent implements OnInit {
+  flag: any = '';
+  ngOnInit() {
+    // this.flag = "start"
+  }
+  onFormSubmit(event: Event) {
+    event.preventDefault(); // Відміна стандартної дії форми
+    console.log("Form submitted", this.flag);
+  }
 }
