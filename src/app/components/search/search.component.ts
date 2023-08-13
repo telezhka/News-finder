975cea5b9baecdfd59a68e1a3d7d507522b6c1aa
Roman Telik <rtelik58@gmail.com>
2023-08-13 22:26:38 +0300

    modal-done

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 import { SearchdataService } from 'src/app/services/searchdata.service';
 import { DatePipe } from '@angular/common';
 
-
+interface SearchResult {
+  image_url: string;
+  title: string;
+  summary: string;
+}
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -12,6 +16,10 @@ import { DatePipe } from '@angular/common';
 export class SearchComponent{
   constructor(private datePipe: DatePipe, private dataService: SearchdataService) { }
   searchResults: any[] = [];
+  isOpen: boolean = false;
+  markup: string = ''
+  result: SearchResult | null = null;
+
   onFormSubmit(searchResult: String) {
     const searchArr = searchResult.split(' ')
     this.dataService.fetchData(searchArr).subscribe(
@@ -32,5 +40,15 @@ export class SearchComponent{
 
   const date = new Date(dateString);
   return this.datePipe.transform(date, 'MMMM d\'th\', yyyy') || '';
-}
+  }
+  openArt(id: any) {
+    this.isOpen = true
+    this.result = this.searchResults.find(option => option.id === id)
+  
+  }
+  closeArt() {
+    // const cont = document.querySelector(".art-cont")
+    this.markup = ''
+    this.isOpen = false
+  }
 }
