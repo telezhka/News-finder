1cae30ef3ca41ce94dcb4835cd96a6ddfa2f186a
Roman Telik <rtelik58@gmail.com>
2023-08-13 15:59:37 +0300

    basic-markup-done

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { SearchdataService } from 'src/app/services/searchdata.service';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  providers: [DatePipe]
 })
 export class SearchComponent{
-  constructor( private dataService: SearchdataService) { }
+  constructor(private datePipe: DatePipe, private dataService: SearchdataService) { }
   searchResults: any[] = [];
   onFormSubmit(searchResult: String) {
     const searchArr = searchResult.split(' ')
@@ -24,4 +25,12 @@ export class SearchComponent{
       }
     );
   }
+  formatDate(dateString: string): string {
+  if (dateString === null) {
+    return '';
+  }
+
+  const date = new Date(dateString);
+  return this.datePipe.transform(date, 'MMMM d\'th\', yyyy') || '';
+}
 }
