d13ecc21236afe429656ef36e699a25388843d17
Roman Telik <rtelik58@gmail.com>
2023-08-13 14:36:01 +0300

    markup

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -8,21 +8,11 @@ import { SearchdataService } from 'src/app/services/searchdata.service';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
-  constructor(private http: HttpClient, private dataService: SearchdataService) { }
-  baseURL: string = 'https://api.spaceflightnewsapi.net/v4/articles/';
+export class SearchComponent{
+  constructor( private dataService: SearchdataService) { }
   searchResults: any[] = [];
-  // flag: any = '';
-  ngOnInit() {
-    // this.flag = ['The', 'most', 'successful', 'IT', 'companies', 'in', '2020']
-  }
   onFormSubmit(searchResult: String) {
     const searchArr = searchResult.split(' ')
-    // console.log("Form submitted", searchResult);
-    // console.log(searchArr);
-    
-    // console.log(`${this.baseURL}?title_contains_one=${searchArr}&summary_contains_one=${searchArr}`);
-    
     this.dataService.fetchData(searchArr).subscribe(
       (results) => {
         this.searchResults = results;
